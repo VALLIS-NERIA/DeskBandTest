4ddc9998c5ffad98f541d2a6f339e90b822c84e1
VALLIS-NERIA <genius-littley@foxmail.com>
2018-07-17 18:17:39 +0800

    prototype 0.1

--- DeskBandRegister/Program.cs
+++ DeskBandRegister/Program.cs
@@ -7,14 +7,22 @@ using System.Threading.Tasks;
 using DeskBandRegister.Properties;
 
 namespace DeskBandRegister {
+    using System.Diagnostics;
+    using System.Threading;
+
     class Program {
-        static void ExtractTool() {
+        static string ExtractTool() {
             var dir = Path.GetTempPath();
             var exePath = dir + "regasm.exe";
             var cfgPath = dir + "regasm.exe.config";
-            if(!File.Exists(exePath)) {
-                using (var s = new FileStream(exePath,FileMode.Create)) {
-                    s.Write(Resources.RegAsm, 0, Resources.RegAsm.Length);
+            if (!File.Exists(exePath)) {
+                using (var s = new FileStream(exePath, FileMode.Create)) {
+                    if (Environment.Is64BitOperatingSystem) {
+                        s.Write(Resources.RegAsm_64, 0, Resources.RegAsm_64.Length);
+                    }
+                    else {
+                        s.Write(Resources.RegAsm_32, 0, Resources.RegAsm_32.Length);
+                    }
                 }
             }
 
@@ -23,11 +31,78 @@ namespace DeskBandRegister {
                     s.Write(Resources.regasm_exe, 0, Resources.regasm_exe.Length);
                 }
             }
+
+            return exePath;
         }
 
-        static void Main(string[] args) {
-            ExtractTool();
+        static void CleanTemp() {
+            var dir = Path.GetTempPath();
+            var exePath = dir + "regasm.exe";
+            var cfgPath = dir + "regasm.exe.config";
+            if (File.Exists(exePath)) {
+                File.Delete(exePath);
+            }
 
+            if (File.Exists(cfgPath)) {
+                File.Delete(cfgPath);
+            }
+        }
+
+        static void Register(string exe, string dll) {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    Arguments = $"/codebase /tlb {dll}",
+                    UseShellExecute = false,
+                    FileName = exe
+                }
+            };
+            process.Start();
+            process.Dispose();
+        }
+
+        static void UnRegister(string exe, string dll) {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    Arguments = $"/u {dll}",
+                    UseShellExecute = false,
+                    FileName = exe
+                }
+            };
+            process.Start();
+            process.Dispose();
+        }
+
+        static void KillExplorer() {
+            var process = new Process
+            {
+                StartInfo =
+                {
+                    Arguments = $"/f /im explorer.exe",
+                    UseShellExecute = false,
+                    FileName = "taskkill"
+                }
+            };
+            process.Start();
+            process.Dispose();
+        }
+        static void Main(string[] args) {
+            if (args.Length < 1) return;
+            CleanTemp();
+            string dll = args[0];
+            var exe = ExtractTool();
+            //UnRegister(exe, dll);
+            //KillExplorer();
+            //Thread.Sleep(5000);
+            //Process.Start("explorer");
+            Register(exe, dll);
+            try {
+                CleanTemp();
+            }
+            catch(UnauthorizedAccessException) { }
         }
     }
 }
--- DiceDeskBand/UserControl1.xaml.cs
+++ DiceDeskBand/UserControl1.xaml.cs
@@ -1,38 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using CSDeskBand;
 using CSDeskBand.Annotations;
-using CSDeskBand.Wpf;
-
 
 namespace DiceDeskBand {
     /// <summary>
     /// UserControl1.xaml 的交互逻辑
     /// </summary>
     [ComVisible(true)]
-    [Guid("89BF6B36-A0B0-4C95-A666-87A55C226986")]
-    [CSDeskBandRegistration(Name = "Sample WPF Deskband")]
+    [Guid("66c84c57-b922-479f-889e-8b3fd3205911")]
+    [CSDeskBandRegistration(Name = "Dice Deskband")]
     public partial class UserControl1 : INotifyPropertyChanged {
-        protected int LastResult { get; set; } = 0;
+        public int LastResult {
+            get => this.lastResult;
+            set { this.lastResult = value; OnPropertyChanged();}
+        }
+
         protected int DiceShape { get; set; } = 6;
         private Random rnd = new Random();
+        private int lastResult = 0;
+
         public UserControl1() {
             InitializeComponent();
+            this.Options.MinHorizontal.Width = 450;
         }
         private void Roll(object sender, RoutedEventArgs e) { this.LastResult = this.rnd.Next(this.DiceShape)+1; }
 
